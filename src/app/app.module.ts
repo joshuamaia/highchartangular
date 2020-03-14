d6d338ca43db0be687ee98aa0c2917f2a6e70e84
Joshua Maia Rodrigues <joshuamaia@codata.pb.gov.br>
2020-03-14 17:10:30 -0300

    Mudando para Angular 9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { TopoComponent } from './topo/topo.component';
-import { BarracomdrilComponent } from './barracomdril/barracomdril.component';
-import { HighchartsChartComponent } from 'highcharts-angular';
-import { BarrabasicoComponent } from './barrabasico/barrabasico.component';
-import { AreabasicoComponent } from './areabasico/areabasico.component';
-import { AreaporcentagemComponent } from './areaporcentagem/areaporcentagem.component';
-import { LinhabasicoComponent } from './linhabasico/linhabasico.component';
-import { LinhacomanotacoesComponent } from './linhacomanotacoes/linhacomanotacoes.component';
-import { PizzabasicoComponent } from './pizzabasico/pizzabasico.component';
-import { PizzacomdrillComponent } from './pizzacomdrill/pizzacomdrill.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
+import { TopoComponent } from "./topo/topo.component";
+import { BarracomdrilComponent } from "./barracomdril/barracomdril.component";
+import { HighchartsChartComponent } from "highcharts-angular";
+import { BarrabasicoComponent } from "./barrabasico/barrabasico.component";
+import { AreabasicoComponent } from "./areabasico/areabasico.component";
+import { AreaporcentagemComponent } from "./areaporcentagem/areaporcentagem.component";
+import { LinhabasicoComponent } from "./linhabasico/linhabasico.component";
+import { LinhacomanotacoesComponent } from "./linhacomanotacoes/linhacomanotacoes.component";
+import { PizzabasicoComponent } from "./pizzabasico/pizzabasico.component";
+import { PizzacomdrillComponent } from "./pizzacomdrill/pizzacomdrill.component";
 
 @NgModule({
   declarations: [
